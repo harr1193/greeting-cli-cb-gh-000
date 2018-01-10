0630bb2f7f48751c5df9881cf811f524f39693fc
Kyle Harris <k_harris95@live.com>
2018-01-10 21:31:03 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,7 @@
 # code the #greeting method here!
-def greeting
-  puts "Hello user please input your name"
-  name = gets.strip
-  puts "Hello #{name}, welcome to this program"
+puts "Please enter your name"
+name = gets.strip
+
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
 end
